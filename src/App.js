86cf09dbeb857edf111cc021432e552c39a98cb4
Jaden Stock <jadenstock@hotmail.com>
2022-12-27 02:13:45 +0200

    React app boilerplate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css"
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Article Manager</h1>
+			<p>The best way to manage your articles</p>
+
+			<div id="1">
+				<h2>Article 1</h2>
+				<p>Article 1 content</p>
+			</div>
+		</div>
+	)
+}
+
+export default App
